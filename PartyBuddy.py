10726a6b2d5b4b1c9fe3f2ac4129bc7aa9801ba1
Yu Jo <zoey5538@gmail.com>
2018-04-19 14:59:38 +0100

    Add functionality to get people who have accepted

--- PartyBuddy.py
+++ PartyBuddy.py
@@ -8,7 +8,7 @@ ask = Ask(app, '/')
 
 
 contacts = {'migle': 'migle19@gmail.com', 'kasia': 'katarzyna.joanna.koprowska@gmail.com', 'yujo': 'zoey5538@gmail.com'}
-partylist = {'kasia': 'katarzyna.joanna.koprowska@gmail.com', 'yujo': 'zoey5538@gmail.com'}
+partylist = {'kasia': 'katarzyna.joanna.koprowska@gmail.com'}
 themeNumber = 20
 
 with open('PartyThemes', encoding='utf8') as p:
@@ -85,13 +85,25 @@ def describe_theme():
 
 @ask.intent('PeopleComing')
 def people_coming():
-    Event.get_attendees_status()
+    emails = Event.get_attendees_status()
+    names = map(lambda email: getKeyByValue(email, contacts), emails)
+    print(names)
+    return statement(', '.join(names) + ' are coming')
+
+
+def getKeyByValue(value, dict):
+    for k, v in dict.items():
+        if v == value:
+            return k
+    return None
+
 
 @ask.intent('SuggestPlaylist')
 def suggest_playlist():
     if themeNumber < 20:
         return statement('I think ' + playlists[themeNumber] + ' will be best for your theme.')
     return statement('You have not chosen the theme yet.')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
